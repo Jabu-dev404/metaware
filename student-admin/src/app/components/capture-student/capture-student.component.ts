75f4fa539c50140c16870d6c8ed05bf326a4d4e9
dudu <dudurose05@gmail.com>
2025-04-10 13:49:11 +0200

    create students

---
+++ student-admin/src/app/components/capture-student/capture-student.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-capture-student',
+  imports: [],
+  templateUrl: './capture-student.component.html',
+  styleUrl: './capture-student.component.css'
+})
+export class CaptureStudentComponent {
+
+}
