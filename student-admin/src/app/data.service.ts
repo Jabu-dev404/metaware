12d1779261ea26c0a4f05492380553c7b3c97ee4
Jabu <sibiyajabu21@gmail.com>
2025-04-10 14:56:05 +0200

    update service

--- student-admin/src/app/data.service.ts
+++ student-admin/src/app/data.service.ts
@@ -3,19 +3,36 @@ import { Degree } from "./models/degree.model";
 
 @Injectable({providedIn :'root'})
 export class DataService{
-    degrees ? : Degree[];  
+  private  degrees = [
+    { 
+        name :" ",
+        noOfYears : 0,
+        modules :[''],
+        falcalty :'',
+        cost : 0
+    }
+  ]  
     option = [{
         id : "o1",
         name : 'Register'
 
     }]
     captureDegree(degree :Degree){
-        this.degrees = [];
+        
         for(let i =0 ; i< degree.modules.length; i++){
             console.log(degree.modules[i]);
           }
           
-        this.degrees?.push(degree)
-        console.log(this.degrees?.length)
+        this.degrees.push(degree)
+        console.log(this.degrees.length)
+
+        this.option.push({
+            id : "o2",
+            name :"check results"
+        })
+    }
+
+    get viewDegree(){
+        return this.degrees
     }
 }
