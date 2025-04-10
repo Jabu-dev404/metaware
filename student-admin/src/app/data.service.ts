a24908066e05b2edd7631280dc919df6df42791b
Jabu <sibiyajabu21@gmail.com>
2025-04-10 10:10:15 +0200

    update service

--- student-admin/src/app/data.service.ts
+++ student-admin/src/app/data.service.ts
@@ -2,8 +2,8 @@ import { Injectable } from "@angular/core";
 import { Degree } from "./models/degree.model";
 
 @Injectable({providedIn :'root'})
-export class dataService{
-    degrees ? : Degree[];
+export class DataService{
+    degrees ? : Degree[];  
     captureDegree(degree :Degree){
         this.degrees?.push(degree)
     }
