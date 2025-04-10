78a5ed11eb74d6db4bcc552a0a1df2f6436bafb9
dudu <dudurose05@gmail.com>
2025-04-10 14:28:09 +0200

    complete capture-student

--- student-admin/src/app/components/capture-student/capture-student.component.ts
+++ student-admin/src/app/components/capture-student/capture-student.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-capture-student',
-  imports: [],
+  imports:[FormsModule],
   templateUrl: './capture-student.component.html',
   styleUrl: './capture-student.component.css'
 })
 export class CaptureStudentComponent {
+  
+  
 
 }
