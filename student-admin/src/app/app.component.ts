16dc680bae692e4d666f3ba5100af453ef218cd6
Peggy <peggydudu5@gmail.com>
2025-04-10 14:45:07 +0200

    Complete

--- student-admin/src/app/app.component.ts
+++ student-admin/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core'
 import { CaptureDegreeComponent } from "./capture-degree/capture-degree.component";
 import { HeaderComponent } from './components/header/header.component';
 import { OptionComponent } from "./components/option/option.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [CaptureDegreeComponent, HeaderComponent, OptionComponent],
+  imports: [RouterOutlet, HeaderComponent, OptionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ student-admin/src/app/components/view-results/view-results.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-results',
+  imports: [],
+  templateUrl: './view-results.component.html',
+  styleUrl: './view-results.component.css'
+})
+export class ViewResultsComponent {
+
+}
