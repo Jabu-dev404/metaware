84b9e8a44cd290157091c3182b02c8ba5fbf78e7
Jabu <sibiyajabu21@gmail.com>
2025-04-11 08:06:43 +0200

    update service

--- student-admin/src/app/data.service.ts
+++ student-admin/src/app/data.service.ts
@@ -1,40 +1,19 @@
 import { Injectable } from "@angular/core";
 import { Degree } from "./models/degree.model";
 
-@Injectable({providedIn :'root'})
-export class DataService{
-//   private  degrees = [
-//     { 
-//         name :" ",
-//         noOfYears : 0,
-//         modules :[''],
-//         falcalty :'',
-//         cost : 0
-//     }
-//   ]  
-
-private degrees : Degree[] =[]
+@Injectable({ providedIn: 'root' })
+export class DataService {
+    private degrees: Degree[] = []
     option = [{
-        id : "o1",
-        name : 'Register'
+        id: "o1",
+        name: 'Register'
 
     }]
-    captureDegree(degree :Degree){
-        
-        for(let i =0 ; i< degree.modules.length; i++){
-            console.log(degree.modules[i]);
-          }
-          
+    captureDegree(degree: Degree) {
         this.degrees.push(degree)
-        console.log(this.degrees.length)
-
-        this.option.push({
-            id : "o2",
-            name :"check results"
-        })
     }
 
-    get viewDegree(){
+    get viewDegree() {
         return this.degrees
     }
 }
