87aa99065b10380fb40d8ed69856fde8ab304c09
Jabu <sibiyajabu21@gmail.com>
2025-04-11 14:44:22 +0200

    update edit degree

--- student-admin/src/app/view-degree/view-degree.component.ts
+++ student-admin/src/app/view-degree/view-degree.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { DataService } from '../data.service';
 import { inject } from '@angular/core/testing';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-view-degree',
-  imports: [],
+  standalone : true,
+  imports: [RouterLink,RouterOutlet],
   templateUrl: './view-degree.component.html',
   styleUrl: './view-degree.component.css'
 })
 export class ViewDegreeComponent {
+  
   constructor(private dataService :DataService){}
 
   get degrees(){
