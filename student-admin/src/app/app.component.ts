647c537e49d3908149890d795a7d2f0d1b397a20
dudu <dudurose05@gmail.com>
2025-04-11 08:03:55 +0200

    create capture-student

--- student-admin/src/app/app.component.ts
+++ student-admin/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core'
 import { CaptureDegreeComponent } from "./capture-degree/capture-degree.component";
 import { HeaderComponent } from './components/header/header.component';
 import { OptionComponent } from "./components/option/option.component";
+import { CaptureStudentComponent } from './components/capture-student/capture-student.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [CaptureDegreeComponent, HeaderComponent, OptionComponent],
+  imports: [CaptureDegreeComponent, HeaderComponent, OptionComponent,CaptureStudentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
