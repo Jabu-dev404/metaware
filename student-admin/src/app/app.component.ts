166545dd32862a10b81a5b95fab2fac079f8705c
Jabu <sibiyajabu21@gmail.com>
2025-04-11 08:01:37 +0200

    update app.component.html,app.component.ts and data service

--- student-admin/src/app/app.component.ts
+++ student-admin/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core'
 import { CaptureDegreeComponent } from "./capture-degree/capture-degree.component";
 import { HeaderComponent } from './components/header/header.component';
 import { OptionComponent } from "./components/option/option.component";
+import { ViewDegreeComponent } from './view-degree/view-degree.component';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports: [CaptureDegreeComponent, HeaderComponent, OptionComponent],
+  imports: [CaptureDegreeComponent, HeaderComponent, OptionComponent,ViewDegreeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
